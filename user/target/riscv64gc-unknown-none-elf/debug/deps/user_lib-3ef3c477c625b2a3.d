271a2738670313c9decde6605b627a2df35d8bea
stellaris <370836525@qq.com>
2022-10-07 23:08:54 +0800

    fix Makefile

---
+++ user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-3ef3c477c625b2a3.d
@@ -0,0 +1,10 @@
+/home/stellaris/operation_system/oscomp-kernel-training-fg-sn2003/user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-3ef3c477c625b2a3.rmeta: src/lib.rs src/console.rs src/syscall.rs src/lang_item.rs src/config.rs src/clone.s
+
+/home/stellaris/operation_system/oscomp-kernel-training-fg-sn2003/user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-3ef3c477c625b2a3.d: src/lib.rs src/console.rs src/syscall.rs src/lang_item.rs src/config.rs src/clone.s
+
+src/lib.rs:
+src/console.rs:
+src/syscall.rs:
+src/lang_item.rs:
+src/config.rs:
+src/clone.s:
